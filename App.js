503de47bbdc40e57eefcc25ddf7126229744b396
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-07-03 17:36:13 -0300

    Installing camera

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar hidden={true} />
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
